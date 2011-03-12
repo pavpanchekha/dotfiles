cecf8a6e64b1048a6db9b2a201e133ee605ec126
Pavel Panchekha <pavpanchekha@gmail.com>
2011-03-12 13:41:19 -0500

    Minor changes

--- usr/lib/run/exts/txt.py
+++ usr/lib/run/exts/txt.py
@@ -3,7 +3,7 @@ import os
 
 run = [
       lambda f, tempdir, args: ["rst2html", f, "--stylesheet=/home/pavpanchekha/usr/lib/run/data/tripoli.simple.css", os.path.join(tempdir, "-out.html")] + args
-    , lambda f, tempdir, args: ["google-chrome", "--app=file://"+os.path.join(tempdir, "-out.html"), "--user-data-dir=/tmp"]
+    , lambda f, tempdir, args: ["luakit", "file://"+os.path.join(tempdir, "-out.html")]
     ]
 
 compile = [
