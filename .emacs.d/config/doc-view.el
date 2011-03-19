1495f581c150b630b47f49df85f7d5b4dddb64bb
Pavel Panchekha <pavpanchekha@gmail.com>
2011-03-19 05:25:34 -0400

    Emacs configuration reorganization

--- .emacs.d/config/doc-view.el
+++ .emacs.d/config/doc-view.el
@@ -3,6 +3,10 @@
 (setq doc-view-continuous t
       doc-view-resolution 192)
 
+(custom-set-variables
+ '(doc-view-continuous t)
+ '(doc-view-resolution 192))
+
 (define-key doc-view-mode-map (kbd "j") 'doc-view-next-line-or-next-page)
 (define-key doc-view-mode-map (kbd "k") 'doc-view-previous-line-or-previous-page)
 (define-key doc-view-mode-map (kbd "h") 'image-backward-hscroll)
