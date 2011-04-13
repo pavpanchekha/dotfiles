c4bd7424a92ab79ca9dcfddfbbfcfff218cd96e3
Pavel Panchekha <pavpanchekha@gmail.com>
2011-04-13 00:35:12 -0400

    Mail changes

--- .config/dwm/bar.sh
+++ .config/dwm/bar.sh
@@ -3,6 +3,18 @@ print() {
     xsetroot -name "$@"
 }
 
+email() {
+    local A
+    A=`cat mail/inbox.spool | grep From\  -c`
+
+    if [[ -z "$A" || "$A" = "0" ]]; then
+        return
+    else
+        echo "| [$A] "
+        return
+    fi
+}
+
 while sleep 3; do
-    print "| `dropbox status` | `date +%H:%M`"
+    print "`email` | `dropbox status` | `date +%H:%M`"
 done
