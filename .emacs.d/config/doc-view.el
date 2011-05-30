7db24162e862d4f34768ca27936032fb14e1abac
Pavel Panchekha <pavpanchekha@gmail.com>
2011-05-30 09:24:32 -0400

    Lots of emacs configs -> literate org-babel file

--- .emacs.d/config/doc-view.el
+++
@@ -1,9 +0,0 @@
-(require 'doc-view)
-
-(setq doc-view-continuous t
-      doc-view-resolution 192)
-
-(define-key doc-view-mode-map (kbd "j") 'doc-view-next-line-or-next-page)
-(define-key doc-view-mode-map (kbd "k") 'doc-view-previous-line-or-previous-page)
-(define-key doc-view-mode-map (kbd "h") 'image-backward-hscroll)
-(define-key doc-view-mode-map (kbd "l") 'image-forward-hscroll)
--- .emacs.d/config/ispell.el
+++
@@ -1,5 +0,0 @@
-(setq ispell-dictionary "american"
-      ispell-highlight-face 'flyspell-incorrect
-      ispell-following-word t
-      ispell-program-name "/usr/bin/ispell"
-      ispell-personal-dictionary "~/.emacs.d/dict")
