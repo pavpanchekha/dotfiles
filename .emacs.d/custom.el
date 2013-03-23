ba18d303ec33406d43f3374ad17ed32605d8af0f
Pavel Panchekha <pavpanchekha@gmail.com>
2013-03-23 14:32:12 -0400

    Evil and Emacs hackery

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,15 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-esc-delay 0)
+ '(evil-want-C-w-in-emacs-state t)
+ '(global-undo-tree-mode nil)
  '(notmuch-saved-searches (quote (("Unread" . "tag:unread") ("Starred" . "tag:flagged") ("Inbox" . "(folder:inbox AND tag:unread) OR tag:inbox")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(jabber-chat-prompt-foreign ((t (:foreground "red"))))
- '(jabber-chat-prompt-local ((t (:foreground "blue"))))
- '(jabber-chat-prompt-system ((t (:foreground "dark green" :weight bold))))
- '(jabber-roster-user-away ((t (:foreground "orange"))))
- '(jabber-roster-user-chatty ((t (:foreground "green"))))
- '(jabber-roster-user-online ((t (:foreground "dark green")))))
+ )
