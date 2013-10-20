cfab2abb6c3347e76ecf64b7a458f57e15d1d730
Pavel Panchekha <pavpanchekha@gmail.com>
2013-10-20 01:52:01 -0700

    Minor config changes.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,8 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(evil-esc-delay 0)
  '(evil-want-C-w-in-emacs-state t)
- '(global-undo-tree-mode nil)
- '(notmuch-saved-searches (quote (("Unread" . "tag:unread") ("Starred" . "tag:flagged") ("Inbox" . "(folder:inbox AND tag:unread) OR tag:inbox")))))
+ '(safe-local-variable-values (quote ((coq-prog-args "-emacs" "-I" "/home/pavpanchekha/Dropbox/Work/CPDT/src/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
