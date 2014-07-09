f938abba5009f67d811bff185a4f439a49b2678e
Pavel Panchekha <me@pavpanchekha.com>
2014-07-08 21:03:23 -0700

    Switch to zenburn color theme

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,6 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(agda2-program-name "/home/.cabal/bin/agda")
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" default)))
  '(evil-esc-delay 0)
  '(evil-want-C-w-in-emacs-state t)
  '(safe-local-variable-values (quote ((coq-prog-args "-emacs" "-I" "/home/pavpanchekha/Dropbox/Work/CPDT/src/")))))
@@ -12,4 +14,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fringe ((t nil)))
+ '(magit-item-highlight ((t nil)) t))
