afe44e80d540221db4ca8d4ef55fe9d2ae6a18b0
Pavel Panchekha <me@pavpanchekha.com>
2014-07-08 21:02:34 -0700

    Agda tweaks

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(agda2-program-name "/home/.cabal/bin/agda")
  '(evil-esc-delay 0)
  '(evil-want-C-w-in-emacs-state t)
  '(safe-local-variable-values (quote ((coq-prog-args "-emacs" "-I" "/home/pavpanchekha/Dropbox/Work/CPDT/src/")))))
