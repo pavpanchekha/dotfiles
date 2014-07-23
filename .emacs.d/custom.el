be65aab1187e83a575d2f91065eef73036340168
Pavel Panchekha <me@pavpanchekha.com>
2014-07-23 00:35:31 -0700

    Emacs config changes (smex, better-defaults, etc)

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,12 +7,18 @@
  '(custom-enabled-themes (quote (zenburn)))
  '(default-input-method "TeX")
  '(dired-listing-switches "-agGh --group-directories-first")
+ '(electric-indent-mode t)
  '(erc-nick "p92")
  '(erc-nick-uniquifier "'")
  '(erc-server "pavpanchekha.com")
  '(custom-safe-themes (quote ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(evil-auto-indent t)
  '(evil-esc-delay 0)
  '(evil-want-C-w-in-emacs-state t)
+ '(fci-rule-color "#073642")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
+ '(magit-diff-refine-hunk (quote all))
  '(proof-electric-terminator-enable t)
  '(split-height-threshold nil)
  '(split-width-threshold 145))
@@ -21,5 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t nil)))
- '(magit-item-highlight ((t nil)) t))
+ '(fringe ((t (:background "#3F3F3F" :foreground "#DCDCCC"))))
+ '(header-line ((t (:background "#1a1a1a" :foreground "#F0DFAF" :box nil))))
+ '(magit-item-highlight ((t nil)))
+ '(vertical-border ((t (:foreground "#1A1A1A")))))
