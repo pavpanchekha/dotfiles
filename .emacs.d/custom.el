e3c1356a4a823b271d7cfe6016d39e861637bfdf
Pavel Panchekha <me@pavpanchekha.com>
2015-02-19 21:29:01 -0800

    Minor changes

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,25 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(agda2-program-name "/home/.cabal/bin/agda")
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
- '(default-input-method "TeX")
- '(dired-listing-switches "-agGh --group-directories-first")
- '(electric-indent-mode t)
- '(erc-nick "p92")
- '(erc-nick-uniquifier "'")
- '(erc-server "pavpanchekha.com")
- '(evil-auto-indent t)
- '(evil-esc-delay 0)
- '(evil-want-C-w-in-emacs-state t)
- '(fci-rule-color "#073642")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
- '(magit-diff-refine-hunk (quote all))
- '(proof-electric-terminator-enable t)
- '(split-height-threshold nil)
- '(split-width-threshold 145))
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default)))
+ '(magit-use-overlays nil))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
