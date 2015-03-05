87d31a11b810847defe8c30115b7701b9c0b50af
Pavel Panchekha <me@pavpanchekha.com>
2015-03-05 09:57:21 -0800

    Some helm changes

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,8 +6,7 @@
  '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
-    ("436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default)))
- '(magit-use-overlays nil))
+    ("4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
