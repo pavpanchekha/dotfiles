82ad3884e698829e96ce6adfdb46f92a8c8166eb
Pavel Panchekha <me@pavpanchekha.com>
2015-09-27 23:36:08 -0700

    Shaving my emacs configuration

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,7 +8,6 @@
    (quote
     ("0c49a9e22e333f260126e4a48539a7ad6e8209ddda13c0310c8811094295b3a3" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default)))
  '(magit-use-overlays nil)
- '(paradox-automatically-star t)
  '(tramp-default-method "ssh")
  '(tramp-inline-compress-start-size nil)
  '(tramp-verbose 2))
