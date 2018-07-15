2c339ccbb1863ce7a528a492422805b55cefb22d
Pavel Panchekha <me@pavpanchekha.com>
2018-07-15 14:11:36 -0700

    Minor emacs changes

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,16 +3,34 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(column-number-mode t)
+ '(compilation-message-face (quote default))
  '(coq-server-user "p92")
- '(custom-enabled-themes (quote (monokai)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
  '(custom-safe-themes
    (quote
-    ("0c49a9e22e333f260126e4a48539a7ad6e8209ddda13c0310c8811094295b3a3" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default)))
+    ("d3a406c5905923546d8a3ad0164a266deaf451856eca5f21b36594ffcb08413a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "0c49a9e22e333f260126e4a48539a7ad6e8209ddda13c0310c8811094295b3a3" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default)))
  '(electric-indent-mode nil)
+ '(fci-rule-color "#515151")
  '(geiser-racket-extra-keywords
    (quote
     ("provide" "require" "unless" "when" "with-handlers" "define/contract" "define-by-match")))
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#3C3D37" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#3C3D37" . 100))))
+ '(magit-diff-use-overlays nil)
  '(magit-push-always-verify nil)
  '(magit-use-overlays nil)
  '(org-entities-user
@@ -21,14 +39,40 @@
  '(org-pretty-entities t)
  '(package-selected-packages
    (quote
-    (tuareg paredit org monokai-theme markdown-mode magit helm-projectile haskell-mode goto-last-change geiser evil-leader esqlite company better-defaults ag)))
+    (color-theme-sanityinc-tomorrow tuareg magit helm-projectile haskell-mode goto-last-change geiser evil-leader esqlite company better-defaults ag)))
  '(paradox-automatically-star t)
+ '(pos-tip-background-color "#FFFACE")
+ '(pos-tip-foreground-color "#272822")
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil)
  '(tramp-default-method "ssh")
  '(tramp-inline-compress-start-size nil)
- '(tramp-verbose 2))
+ '(tramp-verbose 2)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#f2777a")
+     (40 . "#f99157")
+     (60 . "#ffcc66")
+     (80 . "#99cc99")
+     (100 . "#66cccc")
+     (120 . "#6699cc")
+     (140 . "#cc99cc")
+     (160 . "#f2777a")
+     (180 . "#f99157")
+     (200 . "#ffcc66")
+     (220 . "#99cc99")
+     (240 . "#66cccc")
+     (260 . "#6699cc")
+     (280 . "#cc99cc")
+     (300 . "#f2777a")
+     (320 . "#f99157")
+     (340 . "#ffcc66")
+     (360 . "#99cc99"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
