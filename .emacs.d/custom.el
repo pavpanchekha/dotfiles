1f33f355ef767b9ee5826fcc51c811c805906c93
Pavel Panchekha <me@pavpanchekha.com>
2023-12-30 11:33:53 +0800

    New custom.el format

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,37 +8,32 @@
  '(ansi-color-names-vector
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(column-number-mode t)
- '(compilation-message-face (quote default))
+ '(compilation-message-face 'default)
  '(coq-server-user "p92")
- '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-enabled-themes '(sanityinc-tomorrow-eighties))
  '(custom-safe-themes
-   (quote
-    ("d3a406c5905923546d8a3ad0164a266deaf451856eca5f21b36594ffcb08413a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "0c49a9e22e333f260126e4a48539a7ad6e8209ddda13c0310c8811094295b3a3" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default)))
+   '("04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f" "d3a406c5905923546d8a3ad0164a266deaf451856eca5f21b36594ffcb08413a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "0c49a9e22e333f260126e4a48539a7ad6e8209ddda13c0310c8811094295b3a3" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default))
  '(electric-indent-mode nil)
  '(fci-rule-color "#515151")
  '(geiser-racket-extra-keywords
-   (quote
-    ("provide" "require" "unless" "when" "with-handlers" "define/contract" "define-by-match")))
- '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+   '("provide" "require" "unless" "when" "with-handlers" "define/contract" "define-by-match"))
+ '(highlight-changes-colors '("#FD5FF0" "#AE81FF"))
  '(highlight-tail-colors
-   (quote
-    (("#3C3D37" . 0)
+   '(("#3C3D37" . 0)
      ("#679A01" . 20)
      ("#4BBEAE" . 30)
      ("#1DB4D0" . 50)
      ("#9A8F21" . 60)
      ("#A75B00" . 70)
      ("#F309DF" . 85)
-     ("#3C3D37" . 100))))
+     ("#3C3D37" . 100)))
  '(magit-diff-hide-trailing-cr-characters t)
- '(magit-diff-options (quote ("--minimal" "--ignore-space-change")))
+ '(magit-diff-options '("--minimal" "--ignore-space-change"))
  '(magit-diff-paint-whitespace nil)
  '(magit-diff-refine-hunk t)
  '(magit-push-always-verify nil)
  '(magit-use-overlays nil)
- '(org-entities-user
-   (quote
-    (("\\square" "\\square" t "&#x2610;" "[]" "[]" "☐"))))
+ '(org-entities-user '(("\\square" "\\square" t "&#x2610;" "[]" "[]" "☐")))
  '(org-pretty-entities t)
  '(package-selected-packages
    (quote
@@ -49,13 +44,12 @@
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil)
- '(tramp-default-method "ssh" nil (tramp))
+ '(tramp-default-method "ssh")
  '(tramp-inline-compress-start-size nil nil (tramp))
  '(tramp-verbose 2 nil (tramp))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#f2777a")
+   '((20 . "#f2777a")
      (40 . "#f99157")
      (60 . "#ffcc66")
      (80 . "#99cc99")
@@ -72,7 +66,7 @@
      (300 . "#f2777a")
      (320 . "#f99157")
      (340 . "#ffcc66")
-     (360 . "#99cc99"))))
+     (360 . "#99cc99")))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
@@ -82,7 +76,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 90 :width normal))))
+ '(default ((t (:inherit nil :stipple nil :background "#2d2d2d" :foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "PT Mono"))))
  '(cursor ((t (:background "#F8F8F8" :foreground "#f8f8f8" :inverse-video t))))
  '(region ((t (:inverse-video nil))))
  '(tex-verbatim ((t (:inherit fixed-width-serif)))))
