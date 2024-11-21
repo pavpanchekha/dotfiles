fe95f62e8af2d375259aef29b4ef5ce43c4b93ed
Pavel Panchekha <me@pavpanchekha.com>
2024-11-21 15:34:03 -0700

    Emacs changes

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,8 +5,6 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(column-number-mode t)
  '(compilation-message-face 'default)
  '(coq-server-user "p92")
@@ -15,7 +13,6 @@
    '("b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28" "6bdc4e5f585bb4a500ea38f563ecf126570b9ab3be0598bdf607034bb07a8875" "04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f" "d3a406c5905923546d8a3ad0164a266deaf451856eca5f21b36594ffcb08413a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "0c49a9e22e333f260126e4a48539a7ad6e8209ddda13c0310c8811094295b3a3" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "436ae3105bb26b7e3edbd624612ee3ba929fd568d3b3bd1f72e6aa2b0cab1bb7" default))
  '(dired-use-ls-dired nil)
  '(electric-indent-mode nil)
- '(fci-rule-color "#515151")
  '(geiser-racket-extra-keywords
    '("provide" "require" "unless" "when" "with-handlers" "define/contract" "define-by-match"))
  '(highlight-changes-colors '("#FD5FF0" "#AE81FF"))
@@ -28,7 +25,7 @@
      ("#A75B00" . 70)
      ("#F309DF" . 85)
      ("#3C3D37" . 100)))
- '(ispell-program-name "/opt/homebrew/bin/ispell")
+ '(ispell-program-name "/opt/homebrew/bin/aspell")
  '(magit-diff-hide-trailing-cr-characters t)
  '(magit-diff-options '("--minimal" "--ignore-space-change"))
  '(magit-diff-paint-whitespace nil)
@@ -38,38 +35,20 @@
  '(org-entities-user '(("\\square" "\\square" t "&#x2610;" "[]" "[]" "☐")))
  '(org-pretty-entities t)
  '(package-selected-packages
-   '(0x0 yaml-mode rust-mode ace-window projectile racket-mode htmlize exec-path-from-shell markdown-mode org tide request ripgrep rg 0blayout ivy-rich counsel ivy color-theme-sanityinc-tomorrow tuareg magit haskell-mode goto-last-change geiser evil-leader esqlite company better-defaults ag))
+   '(scribble-mode ack 0xc 0x0 yaml-mode rust-mode ace-window projectile racket-mode htmlize exec-path-from-shell markdown-mode org tide request ripgrep rg 0blayout ivy-rich counsel ivy color-theme-sanityinc-tomorrow tuareg magit haskell-mode goto-last-change geiser evil-leader esqlite company better-defaults ag))
  '(paradox-automatically-star t)
  '(pos-tip-background-color "#FFFACE")
  '(pos-tip-foreground-color "#272822")
- '(safe-local-variable-values '((TeX-master . "main")))
+ '(safe-local-variable-values
+   '((variable-pitch-mode)
+     (variable-pitch-mode . f)
+     (TeX-master . "main")))
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil)
  '(tramp-default-method "ssh")
  '(tramp-inline-compress-start-size nil)
  '(tramp-verbose 2 nil (tramp))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   '((20 . "#f2777a")
-     (40 . "#f99157")
-     (60 . "#ffcc66")
-     (80 . "#99cc99")
-     (100 . "#66cccc")
-     (120 . "#6699cc")
-     (140 . "#cc99cc")
-     (160 . "#f2777a")
-     (180 . "#f99157")
-     (200 . "#ffcc66")
-     (220 . "#99cc99")
-     (240 . "#66cccc")
-     (260 . "#6699cc")
-     (280 . "#cc99cc")
-     (300 . "#f2777a")
-     (320 . "#f99157")
-     (340 . "#ffcc66")
-     (360 . "#99cc99")))
- '(vc-annotate-very-old-color nil)
  '(weechat-color-list
    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 
@@ -78,7 +57,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2d2d2d" :foreground "#cccccc" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "PT Mono"))))
- '(cursor ((t (:background "#F8F8F8" :foreground "#f8f8f8" :inverse-video t))))
- '(region ((t (:inverse-video nil))))
  '(tex-verbatim ((t (:inherit fixed-width-serif)))))
